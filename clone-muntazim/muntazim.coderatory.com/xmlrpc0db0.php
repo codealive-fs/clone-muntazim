4d039e7dd295d9f709d270b9307428f04e6da777
Faiz U. Shaikh <61307003+codealive-fs@users.noreply.github.com>
2022-01-31 11:52:53 +0500

    Initial commit

---
+++ clone-muntazim/muntazim.coderatory.com/xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://muntazim.coderatory.com</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://muntazim.coderatory.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://muntazim.coderatory.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://muntazim.coderatory.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://muntazim.coderatory.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://muntazim.coderatory.com/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
